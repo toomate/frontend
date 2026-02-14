852a59e4f7173f3176ef341143ded11bd39d7179
Guilherme <guilhermecamargo1978@gmail.com>
2026-02-14 17:53:04 -0300

    tela padrão

---
+++ src/Boletos.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./index1.css";
+import { Menu } from "lucide-react";
+import "./index1.css";
+
+export default function Index1({ irPara }) {
+  return (
+    <div className="dashboard">
+      <header className="header">
+        <div className="lado-esquerdo">
+          <button className="hamburger-btn">
+            <Menu size={28} color="#b88b09"/>
+          </button>
+
+          <div className="logo-circulo"></div>
+
+          <div className="restaurante">
+            <div className="restaurante-name">Toomate Bistrô</div>
+            <div className="restaurante-subnome">Kaio</div>
+          </div>
+        </div>
+          
+        <button onClick={() => irPara("login")} className="btn">Sair</button>
+      </header>
+    </div>
+  )
+}
