678d994e94d81327d08ab1b395add98aac9f6dcf
Guilherme <guilhermecamargo1978@gmail.com>
2026-02-14 20:16:40 -0300

    boletos v1.2

--- src/Boletos.jsx
+++ src/Boletos.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import "./index1.css";
 import { Menu, CalendarDays, Search } from "lucide-react";
 
+export default function Boletos({ irPara }) {
   return (
     <div className="boletos">
       <header className="header">
         <div className="lado-esquerdo">
           <button className="hamburger-btn">
+            <Menu size={28} color="#b88b09" />
           </button>
 
           <div className="logo-circulo"></div>
 
           <div className="restaurante">
             <div className="restaurante-name">Toomate Bistrô</div>
+            <div className="restaurante-subnome">Usuário 1</div>
           </div>
         </div>
 
