e36babb8e0fedd28c219fc510db30cbb0e4dadb4
GuinhoFSilva <guilherme.silva@sptech.school>
2026-02-20 12:26:01 -0300

    implementando lógica de mensagem no card

--- src/components/CardRelatorio/CardRelatorio.jsx
+++ src/components/CardRelatorio/CardRelatorio.jsx
@@ -3,16 +3,17 @@ import { Button } from "../Button/Button"
 import React from "react"
 import "./CardRelatorio.css"
 
-export function CardRelatorio({ props }) {
+export function CardRelatorio({ props = [] }) {
     console.log(props)
     return (
         <div className="container-card">
             <div className="titulo-relatorio">Relatório de Mudanças</div>
             <div className="produtos">
-                {props.map(atual => <React.Fragment key={atual.id}>
+                {props && props.length > 0 ? (props.map(atual => <React.Fragment key={atual.id}>
                     <div className="produto-linha">
                         <div className="produto">{atual.produto}</div><div className="info-linha">{atual.quantidadeMedida}</div><div className="icone-linha"><X /></div></div>
-                </React.Fragment>)}
+                </React.Fragment>)) :
+                <div className="mensagem-vazio">Não há produtos!</div> }
             </div>
             <div className="botoes">
                 <Button texto={"Editar"} Icone={SquarePen}></Button>
