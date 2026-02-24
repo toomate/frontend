e0c31315db06019444582f66b3f38cdf34be6809
Guilherme <guilhermecamargo1978@gmail.com>
2026-02-24 17:09:04 -0300

    atualização da tela principal

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import Cadastro from "./cadastro";
 import Login from "./login";
 import Index1 from "./Index1";
+import CadastroInsumo from './CadastroInsumo';
+import CadastroLote from './CadastroLote';
+import Boletos from './Boletos';
 import './App.css'
 
 
 function App() {
-  const [tela, setTela] = useState("cadastro");
+  const [tela, setTela] = useState("Index1");
 
   function irPara(t) {
     setTela(t);
@@ -14,9 +17,11 @@ function App() {
 
   return (
     <>
+      {tela === "Index1" && <Index1 irPara={irPara} />}
       {tela === "cadastro" && <Cadastro irPara={irPara} />}
       {tela === "login" && <Login irPara={irPara} />}
       {tela === "dashboard" && <Index1 irPara={irPara} />}
+      {tela === "boletos" && <Boletos irPara={irPara} />}
     </>
   )
 }
