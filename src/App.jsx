2bce97002c7aca7d8661de678019f0ba42592360
lucaspaessptech <lucas.paes@sptech.school>
2026-03-01 15:47:35 -0300

    Adicionando header e arrumando visual do botão do evento no mobile

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,8 @@ import Cadastro from "./Cadastro";
 import CadastroInsumo from "./CadastroInsumo";
 import CadastroLote from "./CadastroLote";
 import Dashboard from "./Dashboard";
-<<<<<<< HEAD
-import Fornecedor from "./Fornecedor";
-import Calendario from "./Calendario";
-=======
 import Calendario from "./components/Calendario/calendario";
 import CalendarioDetail from "./components/Calendario/calendarioDetail";
->>>>>>> a00d9aa (Ao clicar no boleto é redirecionado a outra tela e passa o boleto para essa tela)
 
 import "./App.css";
 import { Estoque } from "./Estoque";
@@ -21,16 +16,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/estoque" element={<Estoque />} />
-        <Route path="/boletos" element={<Boletos />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/cadastro-insumo" element={<CadastroInsumo />} />
-        <Route path="/cadastro-lote" element={<CadastroLote />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/fornecedores" element={<Fornecedor />} />
+         <Route path="/" element={<Login />} />
+         <Route path="/boletos" element={<Boletos />} />
+         <Route path="/cadastro" element={<Cadastro />} />
+         <Route path="/cadastro-insumo" element={<CadastroInsumo />} />
+         <Route path="/cadastro-lote" element={<CadastroLote />} />
+         <Route path="/dashboard" element={<Dashboard />} />
          <Route path="/calendario" element={<Calendario />} />
-         <Route path="/detalhes" element={<CalendarioDetail />} />
+         <Route path="/calendarioDetalhes" element={<CalendarioDetail />} />
       </Routes>
      </BrowserRouter>
   );
