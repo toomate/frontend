356731563b22331dd61e87081e3a3f31c5480672
lucaspaessptech <lucas.paes@sptech.school>
2026-03-07 15:59:51 -0300

    Alteração no script do scanner

--- src/components/Leitor/LeitorScanner.jsx
+++ src/components/Leitor/LeitorScanner.jsx
@@ -4,21 +4,35 @@ import { BrowserMultiFormatReader } from "@zxing/browser";
 export default function BarcodeScanner({ onResult }) {
   const videoRef = useRef(null);
   const codeReader = useRef(null);
+  const controlsRef = useRef(null);
 
   useEffect(() => {
     codeReader.current = new BrowserMultiFormatReader();
 
     codeReader.current
-      .decodeFromVideoDevice(null, videoRef.current, (result, err) => {
+      .decodeFromVideoDevice(null, videoRef.current, (result, err, controls) => {
+
+        if (!controlsRef.current && controls) {
+          controlsRef.current = controls;
+        }
+
         if (result) {
-          
-          console.log("Código detectado:", result.getText());
-          onResult(result.getText());
+          const text = result.getText();
+          console.log("Código detectado:", text);
+
+          onResult(text);
+
+          // para a câmera
+          if (controlsRef.current) {
+            controlsRef.current.stop();
+          }
         }
       });
 
     return () => {
-      codeReader.current.reset();
+      if (controlsRef.current) {
+        controlsRef.current.stop();
+      }
     };
   }, [onResult]);
 
