0a4bb507e5b2cd92514b00491dfd99c984424e7b
lucaspaessptech <lucas.paes@sptech.school>
2026-04-24 21:59:58 -0300

    Alterando entrypoint, env e src/config.js para permitir execução da api do whatsapp tanto local quanto na nuvem

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ const config = {
   API_URL:
     window.env?.API_URL ||
     window.API_URL ||
-    "http://localhost:8080"
+    "http://localhost:8080",
+    VITE_WAHA_API_KEY: window.env?.VITE_WAHA_API_KEY || import.meta.env.VITE_WAHA_API_KEY || "http://localhost:3000"
 }
 
 export default config
